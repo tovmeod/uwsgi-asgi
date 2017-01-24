a072357a624d96fe6c8caff7d11186a999318c8c
avraham <tovmeod@gmail.com>
2017-01-24 15:36:44 +0200

    adding classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import find_packages, setup
-
 from uwsgi_asgi import __version__
 
 setup(
@@ -11,17 +10,34 @@ setup(
     description="ASGI support to UWSGI.",
     long_description=open('README.rst').read(),
     license='BSD',
-    keywords='uwsgi, django, channels, websocket',
-    packages=find_packages(),
+    keywords='uwsgi django channels websocket asgi',
+    packages=find_packages(exclude=['tests']),
     include_package_data=True,
     install_requires=[
-        'uWSGI==2.0.14',
+        'uWSGI>=2.0.14',
         'six',
-        'u-msgpack-python==2.3.0',
+        'u-msgpack-python>=2.3.0',
     ],
-    tests_require=open('testproj/requirements.txt').readlines()+['websocket-client==0.40.0', 'pytest==3.0.5',
-                                                                 'pytest-timeout==1.2.0'],
+    tests_require=open('testproj/requirements.txt').readlines()+['websocket-client>=0.40.0', 'pytest>=3.0.5',
+                                                                 'pytest-timeout>=1.2.0'],
     entry_points={'console_scripts': [
         'uwsgiasgi = uwsgi_asgi.cli:CommandLineInterface.entrypoint',
     ]},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
+        # "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        'Operating System :: POSIX',
+        "Programming Language :: Python",
+        # 'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        "Framework :: Django",
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Server'
+        'Topic :: Internet :: WWW/HTTP :: ASGI :: Server'
+    ]
 )
+
